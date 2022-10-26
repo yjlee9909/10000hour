6c9dd1a3f0c46c93972ca8935f23037222c50c60
yjlee9909 <yjlee9909@naver.com>
2022-10-26 23:40:31 +0900

    JS 추가중

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,18 @@
-// js file
+// 모달 창 처음에 안보이게 처리
+const modal = document.querySelector('.modal');
+modal.style.display = "none";
+
+// 선언
+const main = document.querySelector('.app');
+const formJob = main.querySelector('form p .con1');
+const formHour = main.querySelector('form p .con2');
+const formBtn = main.querySelector('form div .form-btn');
+const goBtn = main.querySelector('.submitted .submitted-go');
+const shareBtn = main.querySelector('.submitted .submitted-share');
+
+
+
+// 이벤트 추가
+formBtn.addEventListener('click', () => {
+    
+})
